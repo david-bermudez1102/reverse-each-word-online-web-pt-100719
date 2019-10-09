d53227f14c33851669dcb6b35be04c1fa319ab2f
David Bermudez <juan.bermudez1102@outlook.com>
2019-10-09 01:05:02 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,7 @@ def reverse_each_word(sentence)
   sentence = sentence.split(" ")
   sentence.collect do |word|
     word = word.reverse
+    puts word
   end
   sentence = sentence.join(" ")
   sentence
